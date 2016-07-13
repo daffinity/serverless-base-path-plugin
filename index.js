cc5822f422089ac0f3c7bb351c72ad6cba6f6186
Aka Kherkeladze <akaki.kherkeladze@gmail.com>
2016-07-13 20:31:30 +0400

    Update index.js
    bugfixes compatibility with serverless 0.56

--- index.js
+++ index.js
@@ -4,83 +4,72 @@
  * Serverless Base Path Plugin
  */
 
-module.exports = function(ServerlessPlugin) {
+module.exports = function (S) {
 
-  const path    = require('path'),
-      fs        = require('fs'),
-      BbPromise = require('bluebird');
-
-  /**
-   * ServerlessPluginBoierplate
-   */
-
-  class ServerlessBasePathPlugin extends ServerlessPlugin {
+    const path = require('path'),
+        fs = require('fs'),
+        BbPromise = require('bluebird');
 
     /**
-     * Constructor
+     * ServerlessPluginBoierplate
      */
 
-    constructor(S) {
-      super(S);
+    class ServerlessBasePathPlugin extends S.classes.Plugin {
+
+        /**
+         * Constructor
+         */
+        constructor() {
+            super();
+            this.name = 'ServerlessBasePath'
+        }
+
+        static getName() {
+            return 'com.daffinity.' + ServerlessBasePathPlugin;
+        }
+
+        registerActions() {
+            S.addAction(this._prefixEndpoints.bind(this), {
+                handler: 'registerHooks',
+                context: 'custom',
+                contextAction: 'run',
+
+            });
+        }
+
+        /**
+         * Register Hooks
+         */
+        registerHooks() {
+            S.addHook(this._prefixEndpoints.bind(this), {
+                action: 'endpointBuildApiGateway',
+                event: 'pre'
+            });
+
+            return BbPromise.resolve();
+        }
+
+        /**
+         * Endpoint Build API Gateway Pre Hook
+         * This adds the basePath from the Component to each Function Endpoint path.
+         */
+        _prefixEndpoints(evt) {
+            let endpoint = S.getProject().getEndpoint(evt.options.name);
+            let basePath = S.getProject().custom.basePath;
+            let _e = endpoint.toObject();
+            let endpointName = `${basePath + _e.path}`;
+
+            if (!basePath) return BbPromise.resolve(evt);
+
+            //set base path to endpoint
+            _e.path = endpointName;
+            endpoint.fromObject(_e);
+            evt.options.name = `${endpointName}~${_e.method}`;
+
+            return BbPromise.resolve(evt);
+        }
     }
 
-    static getName() {
-      return 'com.daffinity.' + ServerlessBasePathPlugin.name;
-    }
-
-    /**
-     * Register Hooks
-     */
-
-    registerHooks() {
-
-      this.S.addHook(this._prefixEndpoints.bind(this), {
-        action: 'endpointBuildApiGateway',
-        event:  'pre'
-      });
-
-      return BbPromise.resolve();
-    }
-
-    /**
-     * Endpoint Build API Gateway Pre Hook
-     * This adds the basePath from the Component to each Function Endpoint path.
-     */
-
-    _prefixEndpoints(evt) {
-      // Validate: Check Serverless version.
-      if (parseInt(this.S._version.split('.')[1]) < 1) {
-        console.log("WARNING: This version of the Serverless Base Path Plugin will not work with a version of Serverless that is less than v0.1.");
-      }
-
-      // Get the component and endpoint.
-      let component = this.S.state.getComponents({ paths: evt.options.path })[0],
-          endpoint = this.S.state.getEndpoints({ paths: evt.options.path })[0];
-
-      // Skip if no basePath is set on the component.
-      // TODO: Store old base paths with the function and remove them if changed or removed in the component config.
-      if (!component.custom || !component.custom.basePath) {
-        return BbPromise.resolve(evt);
-      }
-
-      let basePath = component.custom.basePath;
-
-      // Set the base path for API Gateway endpoint if it isn't already.
-      if (!endpoint.path.startsWith(basePath)) {
-        endpoint.path = basePath + endpoint.path;
-        endpoint.save(function () {
-          BbPromise.resolve(evt);
-        });
-      } else {
-        // Otherwise, skip plugin
-        return BbPromise.resolve(evt);
-      }
-    }
-  }
-
-  // Export Plugin Class
-  return ServerlessBasePathPlugin;
-
+    // Export Plugin Class
+    return ServerlessBasePathPlugin;
 };
-
-
